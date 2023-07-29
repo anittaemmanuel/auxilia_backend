00ec22b7b59207e8fe412f9069617ab74a9927e1
Akhil.A <56401501+2001akhil@users.noreply.github.com>
2023-07-29 20:42:55 +0530

    Update db.js
    db name reupdated

--- dbconnection/db.js
+++ dbconnection/db.js
@@ -4,7 +4,7 @@ var connetion = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "",
-  database: "auxilia",
+  database: "auxillia",
 });
 connetion.connect((err)=>{
      if(err){
